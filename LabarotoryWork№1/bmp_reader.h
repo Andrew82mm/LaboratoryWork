499b539ea2d77099c583c27939fc16604ad324c1
ShadeOfMorr <shcheka.d@gmail.com>
2024-11-14 22:11:53 +0300

    Update bmp_reader.h

--- LabarotoryWork№1/bmp_reader.h
+++ LabarotoryWork№1/bmp_reader.h
@@ -3,7 +3,8 @@
 
 #include "main.h"
 #include <fstream>
-
+// Я думаю, это должно быть не набором функций. Это должен быть класс картинки, которая умеет себя читать
+// и писать, а удаление всех ресурсов должно происходить в ее деструкторе. И что же все-таки за ссылка на двойной указатель?
 bool readBMP(const char *fileName, BITMAPFILEHEADER &fileHeader, BITMAPINFOHEADER &fileInfoHeader, RGBQUAD **&rgbInfo); // Loads a BMP file and stores information in the headers and pixel array
 bool writeBMP(const char *fileName, const BITMAPFILEHEADER &fileHeader, const BITMAPINFOHEADER &fileInfoHeader, RGBQUAD **rgbInfo); // Saves the pixel array and headers to a BMP file
 void cleanupinf(RGBQUAD **&inf, unsigned int height); // Clears the memory allocated for the pixel array
